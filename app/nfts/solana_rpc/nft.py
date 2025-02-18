e45128009880b5563b74acee0283bd7d0534a5e6
Lakshay Garg <lakshaygarg.in@gmail.com>
2025-02-18 16:59:28 +0530

    Enhance Solana NFT data retrieval with detailed account information and base64 encoding

--- app/nfts/solana_rpc/nft.py
+++ app/nfts/solana_rpc/nft.py
@@ -1,5 +1,6 @@
 from solana.rpc.async_api import AsyncClient
 from solders.pubkey import Pubkey
+import base64
 
 RPC_URL = "https://api.mainnet-beta.solana.com"
 
@@ -8,4 +9,16 @@ async def get_nft_data(mint_address: str):
     public_key = Pubkey.from_string(mint_address)
     response = await client.get_account_info(public_key)
     await client.close()
-    return response["result"]["value"]
+
+    if response.value is None:
+        return {"error": "NFT data not found"}
+
+    account_data = {
+        "lamports": response.value.lamports,
+        "owner": str(response.value.owner),
+        "executable": response.value.executable,
+        "rent_epoch": response.value.rent_epoch,
+        "data": base64.b64encode(response.value.data).decode("utf-8") if response.value.data else None
+    }
+
+    return account_data
